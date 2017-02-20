b22381fbc1d4c19d833c3b47d92d94283ed341e2
Olo <jszczep@gmail.com>
2017-02-20 10:16:17 +0100

    additional files and changes for Heroku

--- blog/urls.py
+++ blog/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('posts.urls', namespace='posts'))
 ]
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
